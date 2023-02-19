db9f5583aba9a79b1396cfb8f562dfca52e6e5bf
SAKE CHAITANYA <61873017+sakechaithanya@users.noreply.github.com>
2023-02-19 21:28:07 +0530

    Update assignment2.2.py

--- assignment2.2.py
+++ assignment2.2.py
@@ -1,3 +1,11 @@
+#1
+def last (n):
+    return n[-1]
+def sort_list_last(tuples):
+    return sorted(tuples, key=last)
+print(sort_list_last([(2,5),(1,2),(4,4),(2,3),(2,1)]))
+
+#2q
 d={}
 def convert(x):
     y=x
