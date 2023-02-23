38a75ad3b6ce2ea6db04b6e484376cea5d71e593
SAKE CHAITANYA <61873017+sakechaithanya@users.noreply.github.com>
2023-02-23 13:32:08 +0530

    Update assignment2.2.py

--- assignment2.2.py
+++ assignment2.2.py
@@ -4,6 +4,28 @@ def last (n):
 def sort_list_last(tuples):
     return sorted(tuples, key=last)
 print(sort_list_last([(2,5),(1,2),(4,4),(2,3),(2,1)]))
+#or
+tuple_list =  [(2, 5), (1, 2), (4, 4), (2, 3), (2, 1)]
+list_1 = []
+
+new_list = [5,2,4,3,1]
+for i in range(len(tuple_list)):
+  list_1.append(list(tuple_list[i]))
+  new_list.append(list_1[i][1])
+
+new_list.sort()
+new_list_output = []
+
+for i in range(len(list_1)):
+  for j in range(len(list_1)):
+    if(new_list[i]==list_1[j][1]):
+      new_list_output.append(list_1[j])
+
+final_output = []
+for i in range(len(new_list_output)):
+  final_output.append(tuple(new_list_output[i]))
+
+print(final_output)
 
 #2q
 d={}
